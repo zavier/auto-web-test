37e8ffe07dffd0774edbcf691cffed2b92930c27
zavier <765324639@qq.com>
2026-05-18 09:06:56 +0800

    feat(expense): add expense project adapter with schema and capabilities

---
+++ src/projects/expense/index.ts
@@ -0,0 +1,3 @@
+export { WorkflowTaskSchema, WorkflowSchema } from './tasks.js';
+export type { WorkflowTask, Workflow } from './tasks.js';
+export { getExpenseCapabilities } from './capabilities.js';
