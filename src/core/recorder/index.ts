0f5c86ca0ac29d4231f8d23c61f11733089ceaf7
zavier <765324639@qq.com>
2026-05-18 23:40:30 +0800

    chore: add index.ts exports for template and recorder modules

---
+++ src/core/recorder/index.ts
@@ -0,0 +1,4 @@
+export { WorkflowParameterizer } from './parameterizer.js';
+export { defaultRules } from './rules.js';
+export type { ParameterizeRule } from './rules.js';
+export type { ParamMapping } from './parameterizer.js';
---
+++ src/core/template/index.ts
@@ -0,0 +1,4 @@
+export { TemplateEngine } from './engine.js';
+export { VariableContextBuilder, buildContext } from './context.js';
+export { TemplateError } from './types.js';
+export type { VariableContext, TemplateWorkflow } from './types.js';
